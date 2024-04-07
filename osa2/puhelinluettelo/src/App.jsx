de0bd769a6ed5f6dad62a4098847f6df50d39f83
OskariE <Oskari.Ekholm2@edu.omnia.fi>
2024-04-07 19:58:40 +0300

    puhelinluettelo step8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
-import axios from 'axios'
+import personService from './services/persons'
 
 
 const App = () => {
@@ -14,7 +14,7 @@ const App = () => {
 
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
+    personService.getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -37,7 +37,7 @@ const App = () => {
       name: newName,
       number: newNumber,
     }
-    axios.post('http://localhost:3001/persons', newPerson)
+    personService.create(newPerson)
       .then(response => {
         setPersons(persons.concat(response.data))
         setNewName("")
