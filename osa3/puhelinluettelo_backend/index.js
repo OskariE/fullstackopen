01f1c820cce4c4fe82b7c1c859241b52cfe23fc0
OskariE <Oskari.Ekholm2@edu.omnia.fi>
2024-04-14 15:43:05 +0300

    puhelinluettelo_backend step5

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = 
 [   
     {
@@ -50,6 +52,14 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    person.id = Math.random() * 100
+    persons = persons.concat(person)
+
+    response.json(person)
 })
 
 const PORT = 3001
