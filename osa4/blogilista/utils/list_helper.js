5862aaf90fbbe325b3773c2b32ad791862d28c25
OskariE <Oskari.Ekholm2@edu.omnia.fi>
2024-08-28 08:55:00 +0300

    apufunktioita ja yksikkötestejä step 3,4,5

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,3 +1,4 @@
+const lodash = require('lodash')
 const dummy = (blogs) => {
     return 1
 }
@@ -10,6 +11,43 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (highest, blog) => {
+        if (highest < blog.likes) {
+            highest = blog.likes
+        }
+        return highest
+    }
+    const most = blogs.reduce(reducer, 0)
+    return blogs.filter(blog => blog.likes === most)[0]._id
+}
+
+const mostBlogs = (blogs) => {
+    const amounts = lodash.countBy(blogs.map(blog => blog.author))
+    const mostAuthor = lodash.max(Object.keys(amounts, author => amounts[author]))
+    const max = lodash.max(Object.values(amounts))
+    return { author:mostAuthor, blogs:max }
+}
+
+const mostLikes = (blogs) => {
+    list = {}
+    const reducer = (list, blog) => {
+        author = blog.author
+        if (Object.keys(list).includes(author)) {
+            const likes = list[author]
+            newLikes = likes + blog.likes
+            list[author] = newLikes
+        } else{
+            list[author] = blog.likes;
+        }
+        return list
+    }
+    const total = blogs.reduce(reducer, list)
+    const max = lodash.max(Object.values(total))
+    const maxAuthor = Object.keys(total).find(key => total[key] === max)
+    return { author: maxAuthor, likes: max }
+}
+
 module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
