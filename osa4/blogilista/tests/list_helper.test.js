76f4fe778fef43080f9defd2d0263fadad59600c
OskariE <Oskari.Ekholm2@edu.omnia.fi>
2024-09-06 15:29:59 +0300

    blogilistan laajennus, step 1,2

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -81,7 +81,7 @@ test('dummy returns one', () => {
   assert.strictEqual(result, 1);
 });
 
-describe('total likes', () => {
+describe('sum of likes', () => {
   test('empty list returns zero', () => {
     const result = listHelper.totalLikes(emptyList)
     assert.strictEqual(result, 0)
@@ -98,7 +98,7 @@ describe('total likes', () => {
   });
 });
 
-describe('favorite blog, most blogs and most total likes', () => {
+describe('returning specific author or blog', () => {
   test('returns blog with highest likes', () => {
     const result = listHelper.favoriteBlog(blogs)
     assert.strictEqual(result, blogs[2]._id)
@@ -115,91 +115,124 @@ describe('favorite blog, most blogs and most total likes', () => {
   })
 })
 
-const initialBlogs = [blogs[0], blogs[1], blogs[2]]
-
-beforeEach(async () => {
-  await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[2])
-  await blogObject.save()
-})
-
-describe('backend tests', () => {
-  test('returns right amount of blogs', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body.length, initialBlogs.length)
+describe('with 3 blogs initially saved', () => {
+  const initialBlogs = [blogs[0], blogs[1], blogs[2]]
+  beforeEach(async () => {
+    await Blog.deleteMany({})
+    let blogObject = new Blog(initialBlogs[0])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[1])
+    await blogObject.save()
+    blogObject = new Blog(initialBlogs[2])
+    await blogObject.save()
   })
 
-  test('returned blogs are json', async () => {
-    await api.get('/api/blogs')
-      .expect(200)
-      .expect('Content-Type', /application\/json/)
+  describe('viewing blogs', () => {
+    test('returns right amount of blogs', async () => {
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body.length, initialBlogs.length)
+    })
+
+    test('returned blogs are json', async () => {
+      await api.get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+    })
   })
 
-  test('blogs _id is converted to id', async () => {
-    const response = await api.get('/api/blogs')
-    assert.strictEqual(response.body[0].id, initialBlogs[0]._id)
-  })
+  describe('check blog correct keys/values', () => {
+    test('blogs _id is converted to id', async () => {
+      const response = await api.get('/api/blogs')
+      assert.strictEqual(response.body[0].id, initialBlogs[0]._id)
+    })
 
-  test('a valid blog can be added', async () => {
-    const newBlog = {
-      title: 'testing',
-      author: 'aaaaaa',
-      url: 'd5454545',
-      likes: '6'
-      }
-
-    await api.post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
+    test('likes defaults to 0', async () => {
+      const newBlog = {
+        title: 'likes are 0',
+        author: 'fff',
+        url: 'hhhhh',
+        }
     
-    const response = await api.get('/api/blogs')
-    const contents = response.body.map(r => r.title)
-
-    assert.strictEqual(response.body.length, initialBlogs.length+1)
-    assert(contents.includes('testing'), true)
+      await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      
+      const response = await api.get('/api/blogs')
+      const contents = response.body.map(r => r.likes)
+    
+      assert.strictEqual(response.body.length, initialBlogs.length+1)
+      assert.strictEqual(contents[contents.length-1], 0)
+    })
   })
 
-  test('likes defaults to 0', async () => {
-    const newBlog = {
-      title: 'likes are 0',
-      author: 'fff',
-      url: 'hhhhh',
-      }
-
-    await api.post('/api/blogs')
-      .send(newBlog)
-      .expect(201)
-      .expect('Content-Type', /application\/json/)
-    
-    const response = await api.get('/api/blogs')
-    const contents = response.body.map(r => r.likes)
-
-    assert.strictEqual(response.body.length, initialBlogs.length+1)
-    assert.strictEqual(contents[contents.length-1], 0)
+  describe('adding blogs', () => {
+    test('a valid blog can be added', async () => {
+      const newBlog = {
+        title: 'testing',
+        author: 'aaaaaa',
+        url: 'd5454545',
+        likes: '6'
+        }
+  
+      await api.post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+      
+      const response = await api.get('/api/blogs')
+      const contents = response.body.map(r => r.title)
+  
+      assert.strictEqual(response.body.length, initialBlogs.length+1)
+      assert(contents.includes('testing'), true)
+    })
+  
+    test('blog without url or title wont be added', async () => {
+      const noTitle = {
+        author: 'fff',
+        url: 'hhhhh'
+        }
+      const noUrl = {
+        title: 'likes are 0',
+        author: 'fff'
+        }
+      
+      await api.post('/api/blogs')
+        .send(noTitle)
+        .expect(400)
+  
+      await api.post('/api/blogs')
+        .send(noUrl)
+        .expect(400)
+    })  
   })
 
-  test('blog without url or title wont be added', async () => {
-    const noTitle = {
-      author: 'fff',
-      url: 'hhhhh'
-      }
-    const noUrl = {
-      title: 'likes are 0',
-      author: 'fff'
-      }
-    
-    await api.post('/api/blogs')
-      .send(noTitle)
-      .expect(400)
+  describe('removing or updating blogs', () => {
 
-    await api.post('/api/blogs')
-      .send(noUrl)
-      .expect(400)
+    test('blog can be removed', async () => {
+      await api.delete(`/api/blogs/${initialBlogs[0]._id}`).expect(204)
+  
+      const blogsAfter = await api.get('/api/blogs')
+      assert.strictEqual(blogsAfter.body.length, 2)
+    })
+  
+    test('blog can be updated', async () => {
+      const updatedBlog = {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 400,
+      }
+  
+      const r = await api.put('/api/blogs/5a422a851b54a676234d17f7')
+        .send(updatedBlog)
+        .expect(200)
+  
+      const response = await api.get('/api/blogs')
+      const likes = response.body.map(r => r.likes)
+      assert(likes.includes(400), true)
+    })
   })
 
   after(async () => {
