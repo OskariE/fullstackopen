cc4813b83f5372d038306355140eaa7348f6e643
OskariE <Oskari.Ekholm2@edu.omnia.fi>
2024-10-03 11:06:30 +0300

    blogilistan laajennus, step 6,7

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,53 +1,61 @@
-const blogsRouter = require('express').Router()
-const Blog = require('../models/blog')
-const User = require('../models/user')
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
-      .populate('user', { username: 1, name: 1})
-    response.json(blogs)
-  })
-  
-blogsRouter.post('/', async (request, response) => {
-  const body = request.body
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
 
-  let user = await User.findById(body.user)
-
-  if (user === null) {
-    const users = await User.find({})
-    user = users[0]
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
   }
+  return null;
+};
+
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
+  response.json(blogs);
+});
+
+blogsRouter.post("/", async (request, response) => {
+  const body = request.body;
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "invalid token" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes,
-      user: user._id
-  })
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id,
+  });
 
-  const savedBlog = await blog.save()
-  user.blogs = user.blogs.concat(savedBlog._id)
-  await user.save()
-  response.status(201).json(savedBlog)
-})
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+  response.status(201).json(savedBlog);
+});
 
-blogsRouter.get('/:id', async (request, response) => {
-  const blog = await Blog.findById(request.params.id)
+blogsRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
   if (blog) {
-    response.json(blog)
+    response.json(blog);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
-blogsRouter.delete('/:id', async (request, response) => {
-  await Blog.findByIdAndDelete(request.params.id)
-    response.status(204).end()
-})
+blogsRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
 
-blogsRouter.put('/:id', async (request, response) => {
-  const body = request.body
+blogsRouter.put("/:id", async (request, response) => {
+  const body = request.body;
 
   const newBlog = {
     _id: body.id,
@@ -55,10 +63,12 @@ blogsRouter.put('/:id', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
-  }
+  };
 
-  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlog, { new: true })
-    response.status(200).json(updatedBlog).end()
-})
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, newBlog, {
+    new: true,
+  });
+  response.status(200).json(updatedBlog).end();
+});
 
-module.exports = blogsRouter
+module.exports = blogsRouter;
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,132 +1,138 @@
-const lodash = require('lodash')
-const Blog = require('../models/blog')
-const User = require('../models/user')
+const lodash = require("lodash");
+const Blog = require("../models/blog");
+const User = require("../models/user");
 
 const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url:
-        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0,
-    },
-  ];
-  
-  const blogs = [
-    {
-      _id: '5a422a851b54a676234d17f7',
-      title: 'React patterns',
-      author: 'Michael Chan',
-      url: 'https://reactpatterns.com/',
-      likes: 7,
-      __v: 0,
-    },
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url:
-        'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0,
-    },
-    {
-      _id: '5a422b3a1b54a676234d17f9',
-      title: 'Canonical string reduction',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
-      likes: 12,
-      __v: 0,
-    },
-    {
-      _id: '5a422b891b54a676234d17fa',
-      title: 'First class tests',
-      author: 'Robert C. Martin',
-      url:
-        'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
-      likes: 10,
-      __v: 0,
-    },
-    {
-      _id: '5a422ba71b54a676234d17fb',
-      title: 'TDD harms architecture',
-      author: 'Robert C. Martin',
-      url:
-        'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
-      likes: 0,
-      __v: 0,
-    },
-    {
-      _id: '5a422bc61b54a676234d17fc',
-      title: 'Type wars',
-      author: 'Robert C. Martin',
-      url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-      likes: 2,
-      __v: 0,
-    },
-  ];
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
+
+const blogs = [
+  {
+    _id: "5a422a851b54a676234d17f7",
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+    __v: 0,
+  },
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+  {
+    _id: "5a422b3a1b54a676234d17f9",
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+    __v: 0,
+  },
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0,
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0,
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0,
+  },
+];
 
 const dummy = (blogs) => {
-    return 1
-}
+  return 1;
+};
 
 const totalLikes = (blogs) => {
-    const reducer = (sum, blog) => {
-        return sum + blog.likes
-    }
+  const reducer = (sum, blog) => {
+    return sum + blog.likes;
+  };
 
-    return blogs.reduce(reducer, 0)
-}
+  return blogs.reduce(reducer, 0);
+};
 
 const favoriteBlog = (blogs) => {
-    const reducer = (highest, blog) => {
-        if (highest < blog.likes) {
-            highest = blog.likes
-        }
-        return highest
+  const reducer = (highest, blog) => {
+    if (highest < blog.likes) {
+      highest = blog.likes;
     }
-    const most = blogs.reduce(reducer, 0)
-    return blogs.filter(blog => blog.likes === most)[0]._id
-}
+    return highest;
+  };
+  const most = blogs.reduce(reducer, 0);
+  return blogs.filter((blog) => blog.likes === most)[0]._id;
+};
 
 const mostBlogs = (blogs) => {
-    const amounts = lodash.countBy(blogs.map(blog => blog.author))
-    const mostAuthor = lodash.max(Object.keys(amounts, author => amounts[author]))
-    const max = lodash.max(Object.values(amounts))
-    return { author:mostAuthor, blogs:max }
-}
+  const amounts = lodash.countBy(blogs.map((blog) => blog.author));
+  const mostAuthor = lodash.max(
+    Object.keys(amounts, (author) => amounts[author])
+  );
+  const max = lodash.max(Object.values(amounts));
+  return { author: mostAuthor, blogs: max };
+};
 
 const mostLikes = (blogs) => {
-    list = {}
-    const reducer = (list, blog) => {
-        author = blog.author
-        if (Object.keys(list).includes(author)) {
-            const likes = list[author]
-            newLikes = likes + blog.likes
-            list[author] = newLikes
-        } else{
-            list[author] = blog.likes;
-        }
-        return list
+  list = {};
+  const reducer = (list, blog) => {
+    author = blog.author;
+    if (Object.keys(list).includes(author)) {
+      const likes = list[author];
+      newLikes = likes + blog.likes;
+      list[author] = newLikes;
+    } else {
+      list[author] = blog.likes;
     }
-    const total = blogs.reduce(reducer, list)
-    const max = lodash.max(Object.values(total))
-    const maxAuthor = Object.keys(total).find(key => total[key] === max)
-    return { author: maxAuthor, likes: max }
-}
+    return list;
+  };
+  const total = blogs.reduce(reducer, list);
+  const max = lodash.max(Object.values(total));
+  const maxAuthor = Object.keys(total).find((key) => total[key] === max);
+  return { author: maxAuthor, likes: max };
+};
 
 const blogsInDb = async () => {
-    const blogs = await Blog.find({})
-    return blogs.map(blog => blog.toJSON())
-}
+  const blogs = await Blog.find({});
+  return blogs.map((blog) => blog.toJSON());
+};
 
 const usersInDb = async () => {
-    const users = await User.find({})
-    return users.map(u => u.toJSON())
-  }
+  const users = await User.find({});
+  return users.map((u) => u.toJSON());
+};
 
 module.exports = {
-    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes, blogsInDb, usersInDb, listWithOneBlog, blogs
-}
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+  blogsInDb,
+  usersInDb,
+  listWithOneBlog,
+  blogs,
+};
